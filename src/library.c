19cdbd94275d21a6c2197d4c134f718618960818
A-Wadhwani <wadhwani@purdue.edu>
2020-04-07 02:16:43 -0400

    Commit proj1

--- src/library.c
+++ src/library.c
@@ -65,10 +65,9 @@ int tree_insert(tree_node_t **tree_root, tree_node_t *tree_node){
   }
   int compare_strings = strcmp((*tree_root)->song_name, tree_node->song_name);
   if (compare_strings == 0){
-    free_node(tree_node);
     return DUPLICATE_SONG;
   }
-  if (compare_strings < 0){
+  if (compare_strings > 0){
     return tree_insert(&((*tree_root)->left_child), tree_node);
   }
   return tree_insert(&((*tree_root)->right_child), tree_node);
