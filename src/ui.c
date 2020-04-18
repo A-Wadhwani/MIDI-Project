bd4354106af3d380c4f29a938f00cc73efdc4b77
A-Wadhwani <wadhwani@purdue.edu>
2020-04-17 21:46:06 -0400

    Commit proj1

--- src/ui.c
+++ src/ui.c
@@ -259,6 +259,7 @@ bool compare_strings(const char* string, const char *pattern){
   for (int i = 0; i < strlen(pattern); i++){
     pattern_low[i] = tolower(pattern[i]);
   }
+  printf("%s %s", string_low, pattern_low);
   if (strstr(string_low, pattern_low) == NULL){
     return false;
   }
