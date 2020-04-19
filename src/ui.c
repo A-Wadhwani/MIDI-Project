cfcf81e4b445471d0179f4560afd45b39b9c7edb
A-Wadhwani <wadhwani@purdue.edu>
2020-04-18 21:54:09 -0400

    Commit proj1

--- src/ui.c
+++ src/ui.c
@@ -453,7 +453,7 @@ gboolean draw_cb(GtkDrawingArea *draw_area, cairo_t *painter, gpointer user_data
   if (low_note > 60){
     low_note = 60;
   }
-  guint note_scale = height / (high_note - low_note + 10);
+  guint note_scale = height / (high_note);
   
   // Draws the white box
 
